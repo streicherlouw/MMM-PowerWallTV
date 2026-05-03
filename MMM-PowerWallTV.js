78abbfd3806830a85889767d908c968f696de03e
Streicher Louw <SLouw+aglemu@agl.com.au>
2026-05-03 21:09:18 +1000

    Moved Stats

--- MMM-PowerWallTV.js
+++ MMM-PowerWallTV.js
@@ -158,7 +158,7 @@ Module.register("MMM-PowerWallTV", {
 
     stage.appendChild(this.renderMetric("solar", this.formatPower(this.snapshot.solarPower), "SOLAR"));
     stage.appendChild(this.renderMetric("home", this.formatPower(this.homePowerToDisplay(this.snapshot)), "HOME"));
-    stage.appendChild(this.renderMetric("battery", this.renderBatteryValue(this.snapshot), this.batteryLabel(this.snapshot), true));
+    scene.appendChild(this.renderMetric("battery", this.renderBatteryValue(this.snapshot), this.batteryLabel(this.snapshot), true));
     stage.appendChild(this.renderMetric("grid", this.renderGridValue(this.snapshot), this.gridLabel(this.snapshot), true));
 
     if (this.config.showVehicle && this.hasWallConnector(this.snapshot)) {
